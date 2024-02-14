5d6e68777f65134575f7fff736ae9fb4c75da348
Memdved <mixail.vilyukov@icloud.com>
2024-02-14 17:48:11 +0300

    Add class Game in game.py and add functions __init__, _del__, run, __check_events, __move, __check_logic, __draw

---
+++ game.py
@@ -0,0 +1,59 @@
+"""Файл для работы с pygame"""
+
+
+import pygame as pg
+
+
+class Game:
+    def __init__(self, screen_width: int = 720,
+                 screen_height: int = 480) -> None:
+        """Инициализация объекта игры."""
+        pg.init()
+
+        self.__screen_width: int = screen_width
+        self.__screen_height: int = screen_height
+        self.__screen: pg.Surface = \
+            pg.display.set_mode(
+                (self.__screen_width,
+                 self.__screen_height)
+            )
+
+        self.__color_bg: tuple[int, int, int] = (255, 255, 255)
+
+        self.__fps: int = 60
+        self.__clock: pg.time.Clock = pg.time.Clock()
+        self.__game_run: bool = True
+
+    def __del__(self) -> None:
+        """Очистка памяти по итогу работы"""
+        pg.quit()
+
+    def run(self) -> None:
+        """Запуск игрового цикла игры."""
+
+        while self.__game_run:
+            self.__check_events()
+            self.__move()
+            self.__check_logic()
+            self.__draw()
+
+            self.__clock.tick(self.__fps)
+
+    def __check_events(self) -> None:
+        """Проверка событий в игровом цикле"""
+        event: pg.event.Event
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.__game_run = False
+
+    def __move(self) -> None:
+        """Движение игрока"""
+        pass
+
+    def __check_logic(self) -> None:
+        """Игровая логика"""
+        pass
+
+    def __draw(self) -> None:
+        """Рисование всех объектов"""
+        pass
