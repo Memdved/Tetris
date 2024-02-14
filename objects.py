e2ada0c8d29247eee1960f3cd0588f7052b3619e
Memdved <mixail.vilyukov@icloud.com>
2024-02-14 20:11:13 +0300

    Add Figures class and subclass FigureO

--- objects.py
+++ objects.py
@@ -6,7 +6,27 @@ from random import randint
 
 
 class Figures:
-    """Класс для создания фигур."""
-    def __init__(self) -> None:
+    """Родительский класс фигур для создания фигур."""
+    image_width: int = 48
+    image_height: int = 48
+
+    def __init__(self, color: int = randint(0, 4)) -> None:
         """Инициализация фигуры"""
-        pass
+        self.sprite = pg.transform.scale(
+            pg.image.load(f"sprites/block{color}.png").convert(),
+            (self.image_width, self.image_height)
+        )
+
+
+class FigureO(Figures):
+    __pos: list[
+        tuple[int, int],
+        tuple[int, int],
+        tuple[int, int],
+        tuple[int, int]
+    ] = [(0, 0), (-1, -1), (0, -1), (-1, 0)]
+
+    def __init__(self):
+        super().__init__()
+
+        self.__blocks: list = [self.sprite.get_rect() for _ in range(4)]
