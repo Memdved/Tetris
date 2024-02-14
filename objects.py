2eaeb86b073c9f664725e2c55af0a73b3a99065a
Memdved <mixail.vilyukov@icloud.com>
2024-02-14 21:30:50 +0300

    Add Figure S

--- objects.py
+++ objects.py
@@ -59,3 +59,24 @@ class FigureI(Figures):
             self.__figure_rect.y = self.__figure[_].y * tile
             sc.blit(self.sprite, (self.__figure_rect.x, self.__figure_rect.y))
 
+
+class FigureS(Figures):
+    __pos: list[
+        tuple[int, int],
+        tuple[int, int],
+        tuple[int, int],
+        tuple[int, int]
+    ] = [(0, 0), (-1, -1), (-1, 0), (0, 1)]
+
+    def __init__(self, w):
+        super().__init__()
+
+        self.__figure = [pg.Rect(x + w // 2, y + 1, 1, 1) for x, y in self.__pos]
+        self.__figure_rect = self.sprite.get_rect()
+
+    def draw_figure(self, sc: pg.Surface, tile):
+        for _ in range(4):
+            self.__figure_rect.x = self.__figure[_].x * tile
+            self.__figure_rect.y = self.__figure[_].y * tile
+            sc.blit(self.sprite, (self.__figure_rect.x, self.__figure_rect.y))
+
